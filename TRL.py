0bc0db2c30a55508c64c6601812bb9d3aa219887
Vixaer <justacoin.co@gmail.com>
2018-01-07 09:41:21 +0900

    Removed excess files and cleaned up mnist code

--- TRL.py
+++ TRL.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python
 import sys, os
 
-sys.path.append('/home/xcao')
-
 import tensorflow as tf
 import numpy as np
 from util import TNSR
 
+"""
+Tucker regression layer (trl)
 
+Input:
+	x 		: the input tensor
+	rank 	: hyperparameter. Must be a list that specifies the Tucker rank of the regression tensor W.
+	n_outputs : the number of outputs. A scalar. For exmaple, if one's doing MNIST this is 10.
+"""
 def trl(x, ranks, n_outputs):
-
-	# n_outputs = [2,2,2,2,2,2]
-	# ranks 	= [1,2,2,1]
-	#FIXED_RANK = 3
-
 	weight_initializer = tf.contrib.layers.xavier_initializer()
 	input_shape = x.get_shape().as_list()[1:]
 
 	core,factors = None, None
-	"""
-	if(type(n_outputs) == type([])):
-		core = tf.get_variable("core_higher_{}".format(np.prod(n_outputs)), shape=ranks[1:]+[FIXED_RANK]*len(n_outputs),
-			initializer = weight_initializer)
-		factors = [tf.get_variable("basic_factor_higher_map_{0}_{1}".format(i,e),
-		shape=(input_shape[i],e),
-		initializer = weight_initializer)
-		for (i, e) in enumerate(ranks[1:])
-		]
-	else:
-		"""
 
 	core = tf.get_variable("core_last", shape=ranks, initializer = weight_initializer)
 	factors = [	tf.get_variable("basic_factor_{0}_{1}".format(i,e),
@@ -37,20 +26,8 @@ def trl(x, ranks, n_outputs):
 				for (i, e) in enumerate(input_shape)
 				]
 
-	bias = tf.get_variable("bias_{}".format(np.prod(n_outputs)), shape=(1, np.prod(n_outputs)))
+	bias = tf.get_variable("bias_trl", shape=(1, n_outputs))
 
-	#bias = tf.get_variable("bias", shape=(1, n_outputs))
-	
-	"""
-	if(type(n_outputs) == type([])):
-		for i in range(len(n_outputs)):
-			factors.append(tf.get_variable("higher_factor_{0}_{1}".format(i,np.prod(n_outputs)),
-				shape=(n_outputs[i], FIXED_RANK),
-				initializer = weight_initializer
-				))
-	else:
-	"""
-	# append the last N+1 factor matrix to the list
 	factors.append(tf.get_variable("factor_{}".format(len(ranks)-1),
 			shape=(n_outputs, ranks[-1]),
 			initializer = weight_initializer))
@@ -59,77 +36,47 @@ def trl(x, ranks, n_outputs):
 
 	x_0 = tf.reshape(x, [-1, np.prod(input_shape)])
 
-	w_minus1 = None
-
-	"""
-	if type(n_outputs) == type([]):
-		w_minus1 = tf.reshape(regression_weights, [np.prod(input_shape), np.prod(n_outputs)])
-		return tf.reshape(tf.add(tf.matmul(x_0, w_minus1) ,bias), [-1]+n_outputs)
-	else:
-	"""
-	
-	print(core.get_shape().as_list())
-	for f in factors:
-		print(f.get_shape().as_list())
-	
-
 	w_minus1 = tf.reshape(regression_weights, [np.prod(input_shape),n_outputs])
 	return tf.add(tf.matmul(x_0, w_minus1) ,bias)
-	#return regression(x, regression_weights, input_shape, bias, n_outputs)
 
+"""
+Tensor Train regression layer (ttrl)
+
+Input:
+	x 		: the input tensor
+	ranks 	: hyperparameter. Must be a list that specifies the TT rank of the regression tensor W.
+	n_outputs : the number of outputs. A scalar. For exmaple, if one's doing MNIST this is 10.
+"""
 def ttrl(x, ranks, n_outputs):
 	weight_initializer = tf.contrib.layers.xavier_initializer()
 
-	if(type(n_outputs) == type([])):
-		#n_outputs 	[2,2,2,2,2,2]
-		#ranks     	[1,1,2,2,3,4,3,2,2,1,1]
-		#x 		 	[50,14,14,32]
-		#input_shape[14,14,32,2,2,2,2,2,2]
-		suffix = np.prod(n_outputs)
-		input_shape = x.get_shape().as_list()[1:]+n_outputs
-		bias = tf.get_variable("bias_{}".format(np.prod(n_outputs)), shape=(1, np.prod(n_outputs)))
+	suffix = n_outputs
+	input_shape = x.get_shape().as_list()[1:]
+	bias = tf.get_variable("bias_{}".format(np.prod(n_outputs)), shape=(1, np.prod(n_outputs)))
 
-		cores = []
+	cores = []
 
-		for i in range(len(input_shape)-1):
-			cores.append(tf.get_variable("core_{0}_output_{1}".format(i,suffix), 
-				shape = (ranks[i], input_shape[i], ranks[i+1]), 
-				initializer = weight_initializer))
-
-		cores.append(tf.get_variable("core_{0}_last_output_{1}".format(i,suffix), 
-			shape = (ranks[-2], input_shape[-1], ranks[-1]), 
+	for i in range(1, len(ranks)-1):
+		cores.append(tf.get_variable("core_{0}_output_{1}".format(i,suffix), 
+			shape = (ranks[i-1], input_shape[i-1], ranks[i]), 
 			initializer = weight_initializer))
 
-		#for c in cores:
-		#	print(c.get_shape().as_list())
+	cores.append(tf.get_variable("core_{0}_last_output_{1}".format(i,suffix), 
+		shape=(ranks[-2],n_outputs,ranks[-1]),
+		initializer = weight_initializer))
 
-		regression_weights = TNSR.tt_to_tensor(cores)
-		w_minus1 = tf.reshape(regression_weights, [np.prod(input_shape[:3]), np.prod(n_outputs)])
-		x_0 = tf.reshape(x, [-1, np.prod(x.get_shape().as_list()[1:])])
-		return tf.reshape(tf.add(tf.matmul(x_0, w_minus1) ,bias), [-1]+n_outputs)
+	regression_weights = TNSR.tt_to_tensor(cores)
+	return regression(x, regression_weights, input_shape, bias, n_outputs)
 
-	else:
-		suffix = n_outputs
-		input_shape = x.get_shape().as_list()[1:]
-		#bias = tf.get_variable("bias", shape=(1, n_outputs))
-		bias = tf.get_variable("bias_{}".format(np.prod(n_outputs)), shape=(1, np.prod(n_outputs)))
 
-		cores = []
+"""
+CP regression layer (cprl)
 
-		for i in range(1, len(ranks)-1):
-			cores.append(tf.get_variable("core_{0}_output_{1}".format(i,suffix), 
-				shape = (ranks[i-1], input_shape[i-1], ranks[i]), 
-				initializer = weight_initializer))
-
-		cores.append(tf.get_variable("core_{0}_last_output_{1}".format(i,suffix), 
-			shape=(ranks[-2],n_outputs,ranks[-1]),
-			initializer = weight_initializer))
-
-		regression_weights = TNSR.tt_to_tensor(cores)
-		return regression(x, regression_weights, input_shape, bias, n_outputs)
-
-	#return regression(x, regression_weights, input_shape, bias, n_outputs)
-
+Input:
+	x 		: the input tensor
+	rank 	: hyperparameter. Must be a scalar that specifies the CP rank of the regression tensor W.
+	n_outputs : the number of outputs. A scalar. For exmaple, if one's doing MNIST this is 10.
+"""
 def cprl(x, rank, n_outputs):
 	weight_initializer = tf.contrib.layers.xavier_initializer()
 	input_shape = x.get_shape().as_list()[1:]
@@ -137,8 +84,6 @@ def cprl(x, rank, n_outputs):
 	bias = tf.get_variable("bias_{}".format(np.prod(n_outputs)), shape=(1, np.prod(n_outputs)))
 
 	rank1_tnsrs = []
-
-	print(rank)
 
 	for i in range(rank):
 		rank1_tnsr = []
@@ -160,9 +105,6 @@ def cprl(x, rank, n_outputs):
 
 def regression(x, regression_weights, input_shape, bias, n_outputs):
 
-	print(input_shape)
-	print(x.get_shape().as_list())
-	print(regression_weights.get_shape().as_list())
 	x_0 = tf.reshape(x, [-1, np.prod(input_shape)])
 
 	w_minus1 = None
@@ -172,26 +114,6 @@ def regression(x, regression_weights, input_shape, bias, n_outputs):
 	else:
 		w_minus1 = tf.reshape(regression_weights, [np.prod(input_shape),n_outputs])
 		return tf.add(tf.matmul(x_0, w_minus1) ,bias)
-
-def next_batch(num, data, labels):
-	'''
-	Return a total of `num` random samples and labels. 
-	'''
-	idx = np.arange(0 , len(data))
-	np.random.shuffle(idx)
-	idx = idx[:num]
-	data_shuffle = [data[ i] for i in idx]
-	labels_shuffle = [labels[ i] for i in idx]
-
-	return np.asarray(data_shuffle), np.asarray(labels_shuffle)
-
-def one_hot(lst,num_classes):
-	ret = []
-	for elem in lst:
-		v = [0]*num_classes
-		v[elem-1] = 1
-		ret.append(v)
-	return ret
 
 
 
--- mnist.py
+++ mnist.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 import sys, os
 
-sys.path.append('/home/xcao')
-
 import tensorflow as tf
 import numpy as np
 
@@ -26,15 +24,6 @@ def mode_dot(tensor, matrix, mode):
     return TNSR.fold(res, mode, new_shape)
 
 def main(rank = None, file = None):
-
-	print(rank)
-
-
-	#training = []
-
-	#x = tf.placeholder(tf.float32, shape=[None,3,4,5])
-
-	#out, core, factors, b = trl(x, [6,7,8,9], 10)
 	
 
 	with tf.Session() as sess:
@@ -81,78 +70,20 @@ def main(rank = None, file = None):
 		
 		# relu layer. input of h_pool to W_conv2 then add bias
 		h_conv2 = tf.nn.relu(conv2d(h_pool1,W_conv2) + b_conv2)
-
-		#print(h_conv2.get_shape().as_list()) [None, 14, 14, 32]
 		
 		# second pooling layer then the size will be 7*7*32
 		h_pool2 = max_pool_2x2(h_conv2)
-		#print(h_pool2.get_shape().as_list())
-		
-
-		#x = tf.reduce_mean(h_pool2, reduction_indices=[1, 2], name="avg_pool")
-		#convshape = h_pool2.get_shape().as_list()[1:]
-
-		# assumption that the order of output conv is 3
-		#weight_initializer = tf.contrib.layers.xavier_initializer()
-		#u1 = tf.get_variable("tcl_gap_{}".format(1), shape=[1,convshape[0]],
-		#    initializer = weight_initializer)
-		#u2 = tf.get_variable("tcl_gap_{}".format(2), shape=[1,convshape[1]],
-		#    initializer = weight_initializer)
-		#u3 = tf.get_variable("tcl_gap_{}".format(3), shape=[32,convshape[2]],
-		#    initializer = weight_initializer)
-
-		#h_pool2 = mode_dot(h_pool2,u1,1)
-		#h_pool2 = mode_dot(h_pool2,u2,2)
-		#h_pool2 = mode_dot(h_pool2,u3,3)
-		#h_pool2 = tf.reshape(h_pool2, [-1, np.prod(h_pool2.get_shape().as_list()[1:])])
-
-		#print(h_pool2.get_shape().as_list())
 
 
-		# weight and bias variable
-		#W_fc1 = weight_variable([32,10])
-		W_fc1_gap = weight_variable([32,10])
-		b_fc1 = bias_variable([10])
-		
-		# flatten h_pool2
-		#h_pool2_flat = tf.reshape(h_pool2, [-1, 32])
-
-		# gap h_pool2
-		conv_gap = tf.reduce_mean(h_pool2, reduction_indices=[1, 2], name="avg_pool")
-		
-		# flat result will be relu layer after matmul of [1,7*7*64] x [7*7*64,1024] + [1,1024]
-		#out = tf.matmul(h_pool2_flat, W_fc1) + b_fc1
-		out = tf.matmul(conv_gap, W_fc1_gap) + b_fc1
-
-		#h_fc1 = tf.matmul(tf.reshape(h_conv2, [-1, 14*14*32]), W_fc1) + b_fc1
-		#out = tf.nn.softmax(h_fc1)
-		
-		# place holder
-		keep_prob = tf.placeholder(tf.float32)
-		
-		# dropout of flattened h_fc1 with keep_prob
-		#h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-		
-		# initialized variables of [1024, 10] and bias [10]
-		#W_fc2 = weight_variable([1024, 10])
-		#b_fc2 = bias_variable([10])
-
-
-		# h_pool2.shape -> (50, 7, 7, 32)
-		#out1 = ttrl(h_conv2, rank, [2,2,2,2,2,2]) #[1,75,100,75,1]
-		#out = ttrl(tf.nn.relu(out1), [1,5,5,5,5,5,10,1], 10)
-		#out1 = trl(h_conv2, rank, [2,2,2,2,2,2]) # [60,30,30,60]
-		#out = trl(tf.nn.relu(out1), rank, 10)
-		#out1 = cprl(h_conv2, rank, [2,2,2,2,2,2])
-		#out = cprl(tf.nn.relu(out1), rank, 10)
-
-		#out = ttrl(tf.nn.relu(h_pool2), rank, 10)
+		"""
+		Low rank Tensor Regression Layer
+		ttrl : Tensor Train Regression Layer
+		trl  : Tucker Regression Layer
+		cprl : CP Regression Layer
+		"""
+		out = ttrl(tf.nn.relu(h_pool2), rank, 10)
 		#out = cprl(tf.nn.relu(h_pool2), rank, 10)
 		#out = trl(tf.nn.relu(h_pool2), rank, 10)
-
-
-		# softmax activation function after multing flat_drop with weight varibale w_fc2 add bias
-		#y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 		# cross entropy comparing y_ and y_conv
 		cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=out))
@@ -172,21 +103,14 @@ def main(rank = None, file = None):
 			batch = mnist.train.next_batch(50)
 			if i%100 == 0:
 				train_accuracy = accuracy.eval(feed_dict={
-					x:batch[0], y_: batch[1], keep_prob: 1.0})
+					x:batch[0], y_: batch[1]})
 				file.write("step %d, training accuracy %g\n"%(i, train_accuracy))
-				#training.append(train_accuracy)
-				file.write("step %d, test accuracy %g\n"%(i, accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})))
 		
-			a = sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-	
-			# h_pool2.shape -> (50, 7, 7, 32)
+			a = sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
 
 		file.write("Final test accuracy %g\n"%accuracy.eval(feed_dict={
-			x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+			x: mnist.test.images, y_: mnist.test.labels}))
 
-	#f.write(training)
-	#f.close()
 
 def run(outfilepath, rank, iter):
 	with open(outfilepath,"w+") as f:
@@ -197,43 +121,5 @@ def run(outfilepath, rank, iter):
 
 if __name__ == '__main__':
 
-	#20877 T
-	#20878 CP
-	#20879 TT
+	run("./out/TT[1,1,1,10,1].txt", 	[1,1,1,10,1]		, 1)
 
-	#run("./out/fc_gap_tcl_free.txt", 0, 5)
-	#run("./out/fc_gap_tcl_fix.txt", 0, 5)
-
-	
-	#run("./out/TT[1,1,1,2,1].txt", 		[1,1,1,2,1]			, 5)
-	#run("./out/TT[1,1,1,5,1].txt", 		[1,1,1,5,1]			, 5)
-	#run("./out/TT[1,1,1,10,1].txt", 	[1,1,1,10,1]		, 5)
-
-	"""
-	for j in [1,5,10,20]
-		for i in range(2, 8):
-			run("./out/TT[1,{0},{1},{2},1].txt".format(i,i,j), 	[1,i,i,j,1]		, 5)
-	"""
-
-	#for i in range(8, 33):
-	#	run("./out/TT[1,7,{0},10,1].txt".format(i), 	[1,7,i,10,1]		, 5)
-	
-	
-	"""
-	for i in range(1,8):
-		run("./out/T[{0},{1},{2},10].txt".format(i,i,i), 	[i,i,i,10]		, 5)
-
-	for i in range(8,33):
-		run("./out/T[7,7,{},10].txt".format(i), 	[7,7,i,10]		, 5)
-	"""
-
-
-	
-	"""
-	for i in range(1, 51):
-		run("./out/C{}.txt".format(i), 	i		, 5)
-
-	for i in range(60,211,10):
-		run("./out/C{}.txt".format(i), 	i		, 5)
-	"""
-	run("./out/fc_gap.txt", 0, 5)
